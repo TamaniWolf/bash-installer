eb9a86397ae85842f8ef5e268c92bc150da2fa2b
Tamani Wolf <VinylWolf.mail@gmail.com>
2022-12-13 22:04:27 +0100

    Update clan-run.sh

--- clan-run.sh
+++ clan-run.sh
@@ -21,6 +21,19 @@ else
     exit 1
 fi
 
+DIRECTORY="ClanBot"
+
+if [ -d "$DIRECTORY" ]
+then
+  echo "$DIRECTORY is Downloaded."
+else
+    echo "$DIRECTORY is not Downloaded"
+    echo "Downloading $DIRECTORY..."
+    wget -N "$base_url/$script_install"
+    bash "$root/$script_install"
+    exit 1
+fi
+
 while [ $choice -eq 4 ]; do
 
     echo "1. Run Screen without Auto Restarting ClanBot."
