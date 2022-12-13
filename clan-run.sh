c84ee1178f30268d27b7428be517e13886c93e48
Tamani Wolf <VinylWolf.mail@gmail.com>
2022-12-13 19:16:30 +0100

    Create clan-run.sh

---
+++ clan-run.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+echo "Running ClanBot"
+root=$(pwd)
+choice=4
+
+if hash screen 2>/dev/null
+then
+    echo "Screen installed."
+else
+    echo "Screen is not installed."
+    echo "Installing Prerequisites..."
+    bash "$root/clan_prereq.sh"
+    exit 1
+fi
+
+while [ $choice -eq 4 ]; do
+
+    echo "1. Run Screen without Auto Restarting ClanBot."
+    echo "2. Run Screen with Auto Restarting ClanBot."
+    echo "3. Exit"
+    echo ""
+    echo "Choose:"
+    echo "[1] to Run without Auto restart after crash/stop in Screen."
+    echo "[2] to Run ClanBot with Auto Restart on crash/stop in Screen."
+    read choice
+
+    if [[ $choice -eq 1 ]] ; then
+        echo ""
+        echo "Running ClanBot without auto restart in Screen. Please wait. . ."
+        while :; do screen -dmS clanbot clanbot.js; sleep 5s; done
+        echo "Done"
+    elif [[ $choice -eq 2 ]] ; then
+        echo ""
+        echo "Running ClanBot with auto restart in Screen. Please wait. . ."
+        while :; do screen -dmS clanbot clanbot.js; sleep 5s; done
+        echo "Done"
+    elif [[ "$choice" -eq 3 ]] ; then
+        echo ""
+        echo "Exiting..."
+        cd "$root"
+        exit 0
+    else
+        echo "Invalid choice"
+        echo ""
+        choice=4
+    fi
+done
+
+cd "$root"
+rm "$root/clan-run.sh"
+exit 0
