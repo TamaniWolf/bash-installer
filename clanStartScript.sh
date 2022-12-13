26d993773f2fe2abc4e967d42acf83b809f7ba48
Tamani Wolf <VinylWolf.mail@gmail.com>
2022-12-13 21:38:24 +0100

    Update clanStartScript.sh

--- clanStartScript.sh
+++ clanStartScript.sh
@@ -5,7 +5,7 @@ echo "Downloading the latest installer..."
 root=$(pwd)
 
 rm "$root/clan-menu.sh" 1>/dev/null 2>&1
-wget -N https://github.com/TamaniWolf/bash-installer/blob/main/clan-menu.sh
+wget -N https://raw.githubusercontent.com/TamaniWolf/bash-installer/main/clan-menu.sh
 
 bash clan-menu.sh
 cd "$root"
